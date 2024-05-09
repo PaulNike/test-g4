3918ac0759355bc2b4cbe7e70ef0e6b16cabe025
Yuler Figueroa <yuleralex1@gmail.com>
2024-05-08 22:21:14 -0500

    fix: add other tests

--- src/test/java/com/codigo/unittest/service/impl/EmpresaServiceImplTest.java
+++ src/test/java/com/codigo/unittest/service/impl/EmpresaServiceImplTest.java
@@ -7,13 +7,14 @@ import com.codigo.unittest.dao.EmpresaRepository;
 import com.codigo.unittest.entity.Empresa;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.ResponseEntity;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
@@ -87,7 +88,7 @@ class EmpresaServiceImplTest {
         assertTrue(response.getBody().getEntidad().isPresent());
         assertSame(empresa, response.getBody().getEntidad().get());
     }
-    @Test
+/*    @Test
     void testObtenerEmpresaNullPointer(){
         //ARRANGE
         Long id = 1L;
@@ -96,5 +97,90 @@ class EmpresaServiceImplTest {
 
         //act & assert
         assertThrows(NoSuchElementException.class, () -> empresaService.obtenerEmpresa(id));
+    }*/
+
+    @Test
+    void testObtenerEmpresaNoExistente() {
+        Long id = 1L;
+        when(empresaRepository.findById(id)).thenReturn(Optional.empty());
+        ResponseEntity<BaseResponse> respose = empresaService.obtenerEmpresa(id);
+        assertEquals(Constants.CODE_EMPRESA_NO_EXIST, respose.getBody().getCode());
+        assertEquals(Constants.MSJ_EMPRESA_NO_EXIST, respose.getBody().getMessage());
+        assertTrue(respose.getBody().getEntidad().isEmpty());
+        assertSame(Optional.empty(), respose.getBody().getEntidad());
+    }
+
+    @Test
+    void  testObtenerListaEmpresasExistentes(){
+        List<Empresa> empresas = new ArrayList<>();
+        empresas.add(new Empresa());
+        when(empresaRepository.findAll()).thenReturn(empresas);
+        ResponseEntity<BaseResponse> respose = empresaService.obtenerTodos();
+        assertEquals(Constants.CODE_OK, respose.getBody().getCode());
+        assertEquals(Constants.MSJ_OK, respose.getBody().getMessage());
+        assertTrue(respose.getBody().getEntidad().isPresent());
+        assertSame(empresas, respose.getBody().getEntidad().get());
+    }
+
+    @Test
+    void  testObtenerListaEmpresasVacia(){
+        List<Empresa> empresas = new ArrayList<>();
+        when(empresaRepository.findAll()).thenReturn(empresas);
+        ResponseEntity<BaseResponse> respose = empresaService.obtenerTodos();
+        assertEquals(Constants.CODE_EMPRESA_NO_EXIST, respose.getBody().getCode());
+        assertEquals(Constants.MSJ_EMPRESA_NO_EXIST, respose.getBody().getMessage());
+        assertTrue(respose.getBody().getEntidad().isEmpty());
+        assertSame(Optional.empty(), respose.getBody().getEntidad());
+    }
+
+    @Test
+    void testEmpresaActualizado(){
+        EmpresaRequest request = new EmpresaRequest();
+        Empresa empresa = new Empresa();
+        when(empresaRepository.existsById(anyLong())).thenReturn(true);
+        when(empresaRepository.findById(anyLong())).thenReturn(Optional.of(empresa));
+        when(empresaRepository.save(empresa)).thenReturn(empresa);
+
+        ResponseEntity<BaseResponse> respose = empresaService.actualizar(anyLong(), request);
+
+        assertEquals(Constants.CODE_OK, respose.getBody().getCode());
+        assertEquals(Constants.MSJ_OK, respose.getBody().getMessage());
+        assertTrue(respose.getBody().getEntidad().isPresent());
+        assertSame(empresa, respose.getBody().getEntidad().get());
+    }
+
+    @Test
+    void testEmpresaNoActualizado(){
+        EmpresaRequest request = new EmpresaRequest();
+        when(empresaRepository.existsById(anyLong())).thenReturn(false);
+
+        ResponseEntity<BaseResponse> respose = empresaService.actualizar(anyLong(), request);
+        assertEquals(Constants.CODE_EMPRESA_NO_EXIST, respose.getBody().getCode());
+        assertEquals(Constants.MSJ_EMPRESA_NO_EXIST, respose.getBody().getMessage());
+        assertTrue(respose.getBody().getEntidad().isEmpty());
+        assertSame(Optional.empty(), respose.getBody().getEntidad());
+    }
+
+    @Test
+    void testEmpresaBorrada(){
+        Empresa empresa = new Empresa();
+        when(empresaRepository.existsById(anyLong())).thenReturn(true);
+        when(empresaRepository.findById(anyLong())).thenReturn(Optional.of(empresa));
+        when(empresaRepository.save(empresa)).thenReturn(empresa);
+        ResponseEntity<BaseResponse> respose = empresaService.delete(anyLong());
+        assertEquals(Constants.CODE_OK, respose.getBody().getCode());
+        assertEquals(Constants.MSJ_OK, respose.getBody().getMessage());
+        assertTrue(respose.getBody().getEntidad().isPresent());
+        assertSame(empresa, respose.getBody().getEntidad().get());
+    }
+
+    @Test
+    void testEmpresaNoBorrada(){
+        when(empresaRepository.existsById(anyLong())).thenReturn(false);
+        ResponseEntity<BaseResponse> respose = empresaService.delete(anyLong());
+        assertEquals(Constants.CODE_EMPRESA_NO_EXIST, respose.getBody().getCode());
+        assertEquals(Constants.MSJ_EMPRESA_NO_EXIST, respose.getBody().getMessage());
+        assertTrue(respose.getBody().getEntidad().isEmpty());
+        assertSame(Optional.empty(), respose.getBody().getEntidad());
     }
 }
